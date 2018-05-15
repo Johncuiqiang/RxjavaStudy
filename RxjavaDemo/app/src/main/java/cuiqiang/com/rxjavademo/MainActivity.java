67af9bdab7498e3e63ae6635e8b28aba645de5f1
geguo <geguol@163.com>
2018-05-15 09:42:59 +0800

    demo init

---
+++ RxjavaDemo/app/src/main/java/cuiqiang/com/rxjavademo/MainActivity.java
@@ -0,0 +1,23 @@
+package cuiqiang.com.rxjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+		initData();
+	}
+
+	private void initView() {
+
+	}
+
+	private void initData() {
+
+	}
+}
